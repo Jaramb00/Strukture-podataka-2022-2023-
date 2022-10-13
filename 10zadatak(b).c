27ab1b8b021c14def329ec1aa3b7af74c1f23396
Jaramb00 <jaramb00@fesb.hr>
2022-10-13 15:56:08 +0200

    Zadatak 1

---
+++ 10zadatak(b).c
@@ -0,0 +1,274 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 256
+#define MAX_SIZE 40
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct _VezanaListaEl;
+struct _BinStabloCvor;
+typedef struct _VezanaListaEl *Position;
+typedef struct _BinStabloCvor *StabloPos;
+
+typedef struct _VezanaListaEl
+{
+	char grad[MAX_SIZE];
+	int brojStanovnika;
+	Position next;
+
+}VezanaListaEl;
+
+typedef struct _BinStabloCvor
+{
+	char drzava[MAX_SIZE];
+	StabloPos left;
+	StabloPos right;
+	Position head;
+}BinStabloCvor;
+
+int InputString(char *file);
+StabloPos ReadFromFiles(char *file, StabloPos root);
+int InsertToListSorted(Position current, Position newEl, int brStan);
+Position StvoriNewEl(char *grad, int brojStanovnika);
+int InsertAfter(Position head, Position newEl);
+int InsertGradoviToList(char *gradoviFile, Position head);
+StabloPos Insert(StabloPos root, char *drzava, char *gradoviFile);
+StabloPos StvoriNewCvor(char *drzava, char *gradoviFile);
+int PrintAll(StabloPos root);
+int InOrder(StabloPos root);
+StabloPos NadiPoImenuDrzave(char *drzava, StabloPos root);
+int PrintGradovi(Position head);
+int PrintGradoviSaMinStan(Position head, int minBrStan);
+
+int main() {
+
+	char file[MAX] = "";
+	StabloPos root = NULL;
+	printf("Unesite ime datoteke:");
+	InputString(file);
+	root = ReadFromFiles(file, root);
+	PrintAll(root);
+
+	char Input[MAX]= "", drzava[MAX]= "";
+	int minBrStanovnika;
+	StabloPos current = NULL;
+	do {
+		printf("Upisite drzavu koju zelite pretrazit:\n");
+		InputString(drzava);
+		current = NadiPoImenuDrzave(drzava, root);
+		if (current != NULL) {
+			printf("Unesite donju granicu broja stanovnika gradova:\n");
+			scanf("%d", &minBrStanovnika); getchar();
+			printf("Gradovi sa vise od %d stanovnika:\n", minBrStanovnika);
+			PrintGradoviSaMinStan(current->head, minBrStanovnika);
+		}
+		printf("Za nastavak unesite bilo koju tipku, a za izlaz upisite exit\n");
+		InputString(Input);
+
+	} while (strcmp(Input, "exit") != 0);
+	system("pause");
+
+	return 0;
+}
+
+int InputString(char *str)
+{
+	fgets(str, MAX, stdin);
+
+	if (str[strlen(str) - 1] == '\n')
+		str[strlen(str) - 1] = '\0';
+	return 0;
+}
+
+StabloPos ReadFromFiles(char *file, StabloPos root)
+{
+	
+	FILE *fp = NULL;
+	char buffer[MAX] = { 0 };
+	char drzava[MAX] = { 0 };
+	char gradoviFile[MAX] = { 0 };
+	int result = 0;
+	fp = fopen(file, "r");
+	if (fp == NULL)
+	{
+		perror("Pogreska u otvaranju datoteke");
+		return NULL;
+	}
+	
+	while (!feof(fp))
+	{
+		fgets(buffer, MAX, fp);
+		result=sscanf(buffer, "%s %s", drzava, gradoviFile);
+		if(result == 2)
+			root = Insert(root, drzava, gradoviFile);
+	}
+	
+	fclose(fp);
+	return root;
+}
+
+Position StvoriNewEl(char *grad, int brojStanovnika)
+{
+	Position q = NULL;
+	q = (Position)malloc(sizeof(VezanaListaEl));
+	if (!q) {
+		perror("Pogreska u alokaciji memorije\n");
+		return NULL;
+	}
+	strcpy(q->grad, grad);
+	q->brojStanovnika = brojStanovnika;
+	q->next = NULL;
+
+	return q;
+}
+
+int InsertAfter(Position head, Position newEl)
+{
+	newEl->next = head->next;
+	head->next = newEl;
+	return 0;
+}
+
+int InsertGradoviToList(char *gradoviFile, Position head)
+{
+	Position newEl = NULL;
+	Position current = head;
+	char buffer[MAX] = { 0 };
+	char grad[MAX] = { 0 };
+	int brojSt = 0;
+	int result = 0;
+	FILE *fp = NULL;
+	
+	fp = fopen(gradoviFile, "r");
+	if (fp == NULL) {
+		perror("Pogreska u otvaranju datoteke");
+		return -1;
+	}
+	
+	while (!feof(fp))
+	{
+		fgets(buffer, MAX, fp);
+		result = sscanf(buffer, "%s %d", grad, &brojSt);
+		if (result == 2) {
+			newEl = StvoriNewEl(grad, brojSt);
+			if(!newEl) {
+				fclose(fp);
+				return -2;
+			}
+			InsertToListSorted(current, newEl, brojSt);
+		}
+	}
+	fclose(fp);
+	return 0;
+}
+
+int InsertToListSorted(Position current, Position newEl, int brStan) {
+	while (current->next != NULL && current->next->brojStanovnika > brStan)
+		current = current->next;
+	InsertAfter(current, newEl);
+	return 0;
+}
+
+StabloPos Insert(StabloPos root, char *drzava, char *gradoviFile)
+{	StabloPos temp = NULL;
+	StabloPos node = root;
+	if (node == NULL) {
+		temp = StvoriNewCvor(drzava, gradoviFile);
+		return temp;
+	}
+	else if (strcmp(node->drzava, drzava) > 0) {
+		node->left = Insert(node->left, drzava, gradoviFile);
+	}
+	else if (strcmp(node->drzava, drzava) < 0) {
+		node->right = Insert(node->right, drzava, gradoviFile);
+	}
+	else {
+		printf("Imamo istu drzavu");
+	}
+	return root;
+}
+
+StabloPos StvoriNewCvor(char *drzava,char *gradoviFile)
+{
+	StabloPos q = NULL;
+	q = (StabloPos)malloc(sizeof(BinStabloCvor));
+	if (!q) {
+		perror("Pogreska u alokaciji memorije\n");
+		return NULL;
+	}
+	strcpy(q->drzava, drzava);
+	q->left = NULL;
+	q->right = NULL;
+	q->head = malloc(sizeof(VezanaListaEl));
+	if (!q->head) {
+		perror("Pogreska u alokaciji memorije\n");
+		free(q);
+		return NULL;
+	}
+	else {
+		q->head->next = NULL;
+		q->head->brojStanovnika = 0;
+		strcpy(q->head->grad, "");
+	}
+		
+	InsertGradoviToList(gradoviFile, q->head);
+	return q;
+}
+
+int PrintAll(StabloPos root)
+{
+	InOrder(root);
+	return 0;
+}
+
+int InOrder(StabloPos root)
+{
+	StabloPos temp = root;
+	if (temp != NULL) {
+		InOrder(temp->left);
+		printf("Drzava: %s\n", temp->drzava);
+		PrintGradovi(temp->head);
+		InOrder(temp->right);
+	}
+	return 0;
+}
+
+StabloPos NadiPoImenuDrzave(char *drzava, StabloPos root)
+{
+	StabloPos node = root;
+	if (node == NULL) {
+		return NULL;
+	}
+	else if (strcmp(node->drzava, drzava) > 0) {
+		NadiPoImenuDrzave(drzava, root->left);
+	}
+	else if (strcmp(node->drzava, drzava) < 0) {
+		NadiPoImenuDrzave(drzava, root->right);
+	}
+	else {
+		return node;
+	}
+}
+
+int PrintGradovi(Position head)
+{
+	Position current = head->next;
+	printf(" Grad:                Broj Stanovnika:\n");
+	while (current != NULL) {
+		printf(" %-20.20s %d\n", current->grad, current->brojStanovnika);
+		current = current->next;
+	}
+	return 0;
+}
+
+int PrintGradoviSaMinStan(Position head, int minBrStan)
+{
+	Position current = head->next;
+	printf(" Grad:                Broj Stanovnika:\n");
+	while (current != NULL) {
+		if (current->brojStanovnika >= minBrStan)
+			printf(" %-20.20s %d\n", current->grad, current->brojStanovnika);
+		current = current->next;
+	}
+	return 0;
+}
